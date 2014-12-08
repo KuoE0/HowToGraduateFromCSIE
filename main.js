df55ac2621707cb8aa35f9bcb237250835af7080
KuoE0 <kuoe0.tw@gmail.com>
2014-12-08 11:26:55 +0800

    add paper section

--- main.js
+++ main.js
@@ -8,7 +8,7 @@
 
 $(document).ready(function() {
     $('#fullpage').fullpage({
-		anchors: ['home', 'oral-applying', 'before-oral', 'on-oral', 'congrat'],
+		anchors: ['home', 'oral-applying', 'before-oral', 'on-oral', 'paper', 'congrat'],
 
 		menu: '#menu',
 		scrollingSpeed: 200,
